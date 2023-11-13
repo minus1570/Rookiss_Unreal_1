fd98779b24fae8d9f28090d21ffb7dbc61728a06
minwoo_si <smwbs1570@gmail.com>
2023-11-13 18:21:34 +0900

    Animation&Ui update

--- Rookiss_Tutorial/Source/Rookiss_Tutorial/Rookiss_Tutorial.Build.cs
+++ Rookiss_Tutorial/Source/Rookiss_Tutorial/Rookiss_Tutorial.Build.cs
@@ -8,7 +8,7 @@ public class Rookiss_Tutorial : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
